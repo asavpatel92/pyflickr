7add9ff63b14d263163b33029266907e0ed4534f
Asav Patel <asavpatel92@gmail.com>
2016-09-20 10:35:04 -0700

    refractor database code

--- app.py
+++ app.py
@@ -1,34 +1,15 @@
-import sqlite3
-
+from db.db import DB
 from service.flickr import Flickr
 import settings
 
-
-def create_table(conn, db):
-    with open("db/DDL.sql", "r") as ddl:
-        conn.execute(ddl.read())
-    return
-
-def __ensure_connected(db):
-    return sqlite3.connect(db)
-
-def save_to_db(data):
-    db = getattr(settings, 'DATABASE', 'pyflickr.db')
-    conn = __ensure_connected(db)
-    cursor = conn.cursor()
-    create_table(conn, db)
-    for metadata in data:
-        query = "REPLACE INTO pyflickr (%s) values (%s)" % (', '.join(metadata.keys()), ', '.join('?' * len(metadata)))
-        cursor.execute(query, metadata.values())
-    conn.commit()
-    conn.close()
-
 def main():
+    db_file = getattr(settings, 'DATABASE', 'pyflickr.db')
+    db = DB(db_file)
     start_urls = [ 'https://www.flickr.com/search/?text=new%20york'
                 , 'https://www.flickr.com/search/?text=paris'
                 , 'https://www.flickr.com/search/?text=rome'
                   ]
-    flickr_crawler = Flickr(urls=start_urls, callback=save_to_db)
+    flickr_crawler = Flickr(urls=start_urls, callback=db.save_to_db)
     with flickr_crawler:
         flickr_crawler.crawl()
 
